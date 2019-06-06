aabbbaf5a27f4a9a681393de520152c2ebd525dd
carolinakfk <carolinakfk@yahoo.es>
2019-06-06 14:35:56 -0600

    Merge dev

--- app/src/main/java/com/dts/roadp/ComWS.java
+++ app/src/main/java/com/dts/roadp/ComWS.java
@@ -156,7 +156,7 @@ public class ComWS extends PBase {
         txtUser = new EditText(this,null);
         txtPassword = new EditText(this,null);
 
-		txtVersion.setText("24-Mayo-2019");
+		txtVersion.setText("04-Junio-2019");
 
 		//#CKFK 20190319 Para facilidades de desarrollo se debe colocar la variable debug en true
 		if (gl.debug) {

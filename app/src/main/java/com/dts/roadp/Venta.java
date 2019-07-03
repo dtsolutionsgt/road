45fe6623af4b78475438d1c1072ba6dcc2eb1216
carolinakfk <carolinakfk@yahoo.es>
2019-07-03 09:31:46 -0600

    #CKFK 2019-07-03 09:30 AM     Agregué un enter

--- app/src/main/java/com/dts/roadp/Venta.java
+++ app/src/main/java/com/dts/roadp/Venta.java
@@ -946,6 +946,7 @@ public class Venta extends PBase {
 								}
 							});
 
+
 							dialogBarra.setNegativeButton("No", new DialogInterface.OnClickListener() {
 								public void onClick(DialogInterface dialog, int which) {
 									isDialogBarraShowed = false;

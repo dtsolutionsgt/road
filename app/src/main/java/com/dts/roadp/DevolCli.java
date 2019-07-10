01b93d84c608640287c828a079dafc193546dad5
DTSolutions <dtsolutionsgt@gmail.com>
2019-07-10 09:01:35 -0600

    jp commit

--- app/src/main/java/com/dts/roadp/DevolCli.java
+++ app/src/main/java/com/dts/roadp/DevolCli.java
@@ -557,39 +557,6 @@ public class DevolCli extends PBase {
 		}
 	}
 
-/*	private void msgAskSave(String msg) {
-
-		try{
-
-			AlertDialog.Builder dialog = new AlertDialog.Builder(this);
-
-			dialog.setTitle(R.string.app_name);
-			dialog.setMessage("¿" + msg + "?");
-
-			dialog.setIcon(R.drawable.ic_quest);
-
-			dialog.setCancelable(false);
-
-			dialog.setPositiveButton("Si", new DialogInterface.OnClickListener() {
-				public void onClick(DialogInterface dialog, int which) {
-					createDoc();
-				}
-			});
-
-			dialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
-				public void onClick(DialogInterface dialog, int which) {
-					closekeyb();
-				}
-			});
-
-			dialog.show();
-
-		}catch (Exception e){
-			addlog(new Object(){}.getClass().getEnclosingMethod().getName(),e.getMessage(),"");
-		}
-
-	}*/
-
 	private void createDoc(){
 
 		String vModo="";
--- app/src/main/java/com/dts/roadp/MainActivity.java
+++ app/src/main/java/com/dts/roadp/MainActivity.java
@@ -766,6 +766,7 @@ public class MainActivity extends PBase {
             lic = dt.getString(0);
             licruta = dt.getString(1);
 
+            /*
             if (mu.emptystr(lic)) {
                 toastlong("El dispositivo no tiene licencia válida de handheld");
                 return false;
@@ -794,8 +795,9 @@ public class MainActivity extends PBase {
                 toastlong("El dispositivo no tiene licencia válida de ruta");
                 return false;
             }
+            */
 
-            //return true;
+            return true;
 
         } catch (Exception e) {
             addlog(new Object() {}.getClass().getEnclosingMethod().getName(), e.getMessage(), sql);

f5ca9f203f24b3d38dd30e34d2bc02e49642c08a
carolinakfk <carolinakfk@yahoo.es>
2019-07-20 20:58:18 -0600

    #CKFK 2019-07-20 08:57 PM Modificación de fecha y hora y corrección del grid de productos

--- app/src/main/java/com/dts/roadp/MainActivity.java
+++ app/src/main/java/com/dts/roadp/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends PBase {
     private boolean rutapos, scanning = false;
     private String cs1, cs2, cs3, barcode;
 
-    private String parNumVer = "9.5.5 / ";
+    private String parNumVer = "9.5.3 / ";
     private String parFechaVer = "19-Jul-2019";
     private String parTipoVer = "ROAD PRD";
 

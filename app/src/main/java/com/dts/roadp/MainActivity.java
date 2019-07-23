50dccca0db692fe3b3737f2577d443a6bc11d1d1
carolinakfk <carolinakfk@yahoo.es>
2019-07-23 16:09:45 -0600

    #CKFK 2019-07-23 04:09 PM Actualizacion de fecha y de version

--- app/src/main/java/com/dts/roadp/MainActivity.java
+++ app/src/main/java/com/dts/roadp/MainActivity.java
@@ -41,8 +41,8 @@ public class MainActivity extends PBase {
     private boolean rutapos, scanning = false;
     private String cs1, cs2, cs3, barcode;
 
-    private String parNumVer = "9.4.45 / ";
-    private String parFechaVer = "22-Jul-2019";
+    private String parNumVer = "9.4.46 / ";
+    private String parFechaVer = "23-Jul-2019";
     private String parTipoVer = "ROAD PRD";
 
     @Override

977c6c61909278a9809c94a6a9c1a912e4a632ee
DTSolutions <dtsolutionsgt@gmail.com>
2019-10-24 09:31:50 -0600

    jp commit

--- app/src/main/java/com/dts/roadp/MainActivity.java
+++ app/src/main/java/com/dts/roadp/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends PBase {
     private String cs1, cs2, cs3, barcode;
 
     private String parNumVer = "9.5.16 / ";
-    private String parFechaVer = "22-10-2019";
+    private String parFechaVer = "24-10-2019";
     private String parTipoVer = "ROAD PRD";
 
 

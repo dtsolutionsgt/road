7414264e100a706f0dbca7968073d8b8b407de64
Carolina Fuentes <carolinakfk@hotmail.com>
2021-06-30 14:05:41 -0600

    #CKFK 2021-06-17 Creación de pantalla de despachos

--- app/src/main/java/com/dts/roadp/appGlobals.java
+++ app/src/main/java/com/dts/roadp/appGlobals.java
@@ -13,7 +13,7 @@ public class appGlobals extends Application {
 
 	public String ruta,rutanom,rutasup,sucur,rutatipo,rutatipog,vend,vendnom,gstr,gstr2,prod,um,umpres,umstock,cliente,clitipo;
 	public String ubas,emp, empnom,imgpath,umpeso,lotedf,impresora, tipoImpresora, codSupervisor, ayudante, ayudanteID, vehiculo, vehiculoID;
-	public String wsURL,bonprodid,bonbarid,bonbarprod,pprodname,contrib,ateninistr,tcorel,URLtemp,URLLocal,URLRemoto;
+	public String wsURL,bonprodid,bonbarid,bonbarprod,pprodname,contrib,ateninistr,tcorel,URLtemp,URLLocal,URLRemoto, iddespacho;
 	public int itemid,gint,tipo,nivel,prodtipo,prw,boldep,vnivel,vnivprec,media;
 	public int autocom,pagomodo,filtrocli,prdlgmode;
 	public long nuevaFecha,atentini;

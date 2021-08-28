b5cd31255a61254bdce4c8af0d4f451a970f73b4
DTSolutions <dtsolutionsgt@gmail.com>
2021-08-28 11:36:35 -0600

    pedidos  fecha 28/8 despues de la revision, cambio a la version 9.5.9

--- app/src/main/java/com/dts/roadp/MainActivity.java
+++ app/src/main/java/com/dts/roadp/MainActivity.java
@@ -44,7 +44,7 @@ public class MainActivity extends PBase {
     private boolean rutapos, scanning = false;
     private String cs1, cs2, cs3, barcode;
 
-    private String parNumVer = "9.5.8 / ";
+    private String parNumVer = "9.5.9 / ";
     private String parFechaVer = "27-08-2021";
     private String parTipoVer = "ROAD QAS";
 

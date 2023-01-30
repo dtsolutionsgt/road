317e8963bebea11923e55f67e824ad38c4638c41
Carolina Fuentes <carolinakfk@hotmail.com>
2023-01-30 01:16:47 -0600

    #CKFK20230129 Impresión del QR con la DataMax

--- app/src/main/java/com/dts/roadp/MainActivity.java
+++ app/src/main/java/com/dts/roadp/MainActivity.java
@@ -45,8 +45,8 @@ public class MainActivity extends PBase {
     private boolean rutapos, scanning = false;
     private String cs1, cs2, cs3, barcode;
 
-    private String parNumVer = "9.7.1 / ";
-    private String parFechaVer = "26-01-2023";
+    private String parNumVer = "9.7.2 / ";
+    private String parFechaVer = "29-01-2023";
     private String parTipoVer = "ROAD QAS";
 
     @Override

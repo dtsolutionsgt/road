15f9361da5d35896a34e5ec583197a074dcc9ed6
Carolina Fuentes <carolinakfk@hotmail.com>
2023-03-08 18:23:12 -0600

    #CKFK202300308 Cambio de versión

--- app/src/main/java/com/dts/roadp/MainActivity.java
+++ app/src/main/java/com/dts/roadp/MainActivity.java
@@ -45,8 +45,8 @@ public class MainActivity extends PBase {
     private boolean rutapos, scanning = false;
     private String cs1, cs2, cs3, barcode;
 
-    private String parNumVer = "9.7.7 / ";
-    private String parFechaVer = "07-03-2023";
+    private String parNumVer = "9.7.8 / ";
+    private String parFechaVer = "08-03-2023";
     private String parTipoVer = "ROAD QAS";
 
     @Override
